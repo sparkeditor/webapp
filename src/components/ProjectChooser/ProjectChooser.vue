ba6563bd98ddef29139cb3f936f4e4267a8f0350
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-03-02 17:48:32 -0700

    Style project chooser

--- src/components/ProjectChooser/ProjectChooser.vue
+++ src/components/ProjectChooser/ProjectChooser.vue
@@ -1,10 +1,17 @@
 <template>
     <Container>
         <h1>Choose Project</h1>
-        <div v-if="credentials && credentials.username && credentials.password">
+        <div class="projectChooser" v-if="credentials && credentials.username && credentials.password">
             <ul>
                 <li v-for="project in projects">
-                    <button @click.prevent="handleProjectClick(project)">{{ project.name }}</button>
+                    <button class="projectButton"
+                            @click.prevent="handleProjectClick(project)"
+                            @mouseover="setActive(project)"
+                            @mouseleave="clearActive()"
+                    >
+                        <i class="fa fa-lg" :class="folderClass(project)" aria-hidden="true"></i>
+                        <span>{{ project.name }}</span>
+                    </button>
                 </li>
             </ul>
         </div>
@@ -22,18 +29,63 @@
      components: {
          Container
      },
-     computed: mapState([
-         'credentials',
-         'projects'
-     ]),
+     computed: {
+         ...mapState([
+             'credentials',
+             'projects'
+         ])
+     },
+     data() {
+         return {
+             active: null
+         }
+     },
      methods: {
+         folderClass(project) {
+             return {
+                 "fa-folder": this.active !== project,
+                 "fa-folder-open": this.active === project
+             }
+         },
          handleProjectClick(project) {
              this.$store.commit("setCurrentProject", project);
              this.$router.push("/editor");
+         },
+         setActive(project) {
+             this.active = project;
+         },
+         clearActive() {
+             this.active = null;
          }
      }
  }
 </script>
 <style lang="scss" scoped>
-
+ @import "../../scss/colors.scss";
+ 
+ .projectChooser {
+     margin: auto;
+     width: 350px;
+ }
+ ul {
+     list-style: none;
+     padding-left: 0;
+ }
+ .projectButton {
+     width: 100%;
+     box-sizing: border-box;
+     text-align: left;
+     box-shadow: none;
+     color: $cream;
+     line-height: 2;
+     border: none;
+     background-color: $slate;
+ }
+ .projectButton i, .projectButton span {
+     color: $cream;
+ }
+ .projectButton:hover {
+     background-color: $teal;
+     cursor: pointer;
+ }
 </style>
