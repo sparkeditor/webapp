116bdc958c1484bb45df4d1abdc29320005cd0a1
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-04-18 23:42:01 -0600

    Formatting

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -23,173 +23,199 @@
     </div>
 </template>
 <script>
- import {mapState} from 'vuex';
- import path from "path";
- import TreeView from "../TreeView";
- import TabbedWindow from "../TabbedWindow";
- import FileMenu from "../FileMenu";
- import PopupDialog from "../PopupDialog";
- import io from "../../socketClient";
- import statusCodes from "../../statusCodes";
- 
- export default {
-     name: 'App',
-     components: {
-         TreeView,
-         TabbedWindow,
-         FileMenu,
-         PopupDialog
-     },
-     computed: {
-         initialSaveAsName() {
-             if (!this.currentFile) {
-                 return "";
-             }
-             const filepath = this.currentFile.path;
-             const projectPath = this.$store.state.projectInfo.rootDirectory.path;
-             this.delimiter = filepath.substr(filepath.search(projectPath), filepath.search(projectPath) + projectPath.length);
-             return filepath.substr(filepath.search(projectPath) + projectPath.length + 1);
-         },
-         ...mapState([
-             'currentFile'
-         ])
-     },
-     data() {
-         const self = this;
-         return {
-             confirmMessage: false,
-             menu: {
-                 File: {
-                     // TODO on create new, open a new 'untitled' buffer
-                     "New File": () => console.log("new file"),
-                     "New Directory": () => console.log("new directory"),
-                     // TODO if current buffer is 'untitled', do a save as instead
-                     "Save": self.saveCurrentFile,
-                     "Save As": () => {
-                         self.showSaveAs = true;
-                     }
-                 },
-                 Edit: {
-                     "Undo": () => console.log("undo"),
-                     "Redo": () => console.log("redo")
-                 }
-             },
-             saveAsMessage: "",
-             showSaveAs: false,
-             initialSaveAsButtons: [
-                 {
-                     text: 'Cancel',
-                     onclick: function() {
-                         self.saveAsName = self.initialSaveAsName;
-                         self.showSaveAs = false;
-                     }
-                 },
-                 {
-                     text: 'Save',
-                     onclick: function() {
-                         switch(self.saveAsName) {
-                             case "":
-                                 self.saveAsMessage = "Path cannot be blank";
-                                 break;
-                             case self.initialSaveAsName:
-                                 self.saveCurrentFile();
-                                 self.showSaveAs = false;
-                                 break;
-                             default:
-                                 // Check if the path is valid
-                                 const pathDir = path.dirname(self.saveAsName);
-                                 let dirExists = false;
-                                 let fileExists = false;
-                                 const projectPath = self.$store.state.projectInfo.rootDirectory.path;
-                                 self.$store.state.projectInfo.mapProjectInfo((fileObject) => {
-                                     const fullDirPath = pathDir === "." ? projectPath : projectPath + self.delimiter + pathDir;
-                                     if (fileObject.path === fullDirPath) {
-                                         dirExists = true;
-                                     }
-                                     if (fileObject.path === projectPath + self.delimiter + self.saveAsName) {
-                                         fileExists = true;
-                                     }
-                                 });
-                                 // If the directory does not exist, show an error
-                                 if (!dirExists) {
-                                     self.saveAsMessage = "Path does not point to a valid directory. You may need to create a new directory.";
-                                     break;
-                                 }
-                                 // If the file already exists, ask for confirmation
-                                 self.confirmMessage = true;
-                                 self.saveAsButtons = [
-                                     {
-                                         text: 'Cancel',
-                                         onclick: function() {
-                                             self.saveAsName = self.initialSaveAsName;
-                                             self.showSaveAs = false;
-                                             self.saveAsButtons = self.initialSaveAsButtons;
-                                             self.confirmMessage = false;
-                                         }
-                                     },
-                                     {
-                                         text: 'Confirm',
-                                         onclick: function() {
-                                             self.saveFileAs(self.saveAsName);
-                                             self.showSaveAs = false;
-                                             self.saveAsButtons = self.initialSaveAsButtons;
-                                             self.confirmMessage = false;
-                                         }
-                                     }
-                                 ];
-                                 break;
-                         }
-                     }
-                 }
-             ],
-             saveAsButtons: [], 
-             saveAsName: self.initialSaveAsName,
-             delimiter: "/"
-         }
-     },
-     methods: {
-         saveCurrentFile() {
-             const self = this;
+import { mapState } from "vuex";
+import path from "path";
+import TreeView from "../TreeView";
+import TabbedWindow from "../TabbedWindow";
+import FileMenu from "../FileMenu";
+import PopupDialog from "../PopupDialog";
+import io from "../../socketClient";
+import statusCodes from "../../statusCodes";
 
-             io.emit("write", 
-                 { 
-                     file: self.currentFile.path,
-                     credentials: self.$store.state.credentials  
-                 },
-                 (response) => {
-                     if (response.status !== statusCodes.OKAY) {
-                         console.error(response);
-                     }
-                 });
-         },
-         saveFileAs(filename) {
-             const self = this;
+export default {
+    name: "App",
+    components: {
+        TreeView,
+        TabbedWindow,
+        FileMenu,
+        PopupDialog
+    },
+    computed: {
+        initialSaveAsName() {
+            if (!this.currentFile) {
+                return "";
+            }
+            const filepath = this.currentFile.path;
+            const projectPath = this.$store.state.projectInfo.rootDirectory
+                .path;
+            this.delimiter = filepath.substr(
+                filepath.search(projectPath),
+                filepath.search(projectPath) + projectPath.length
+            );
+            return filepath.substr(
+                filepath.search(projectPath) + projectPath.length + 1
+            );
+        },
+        ...mapState(["currentFile"])
+    },
+    data() {
+        const self = this;
+        return {
+            confirmMessage: false,
+            menu: {
+                File: {
+                    // TODO on create new, open a new 'untitled' buffer
+                    "New File": () => console.log("new file"),
+                    "New Directory": () => console.log("new directory"),
+                    // TODO if current buffer is 'untitled', do a save as instead
+                    Save: self.saveCurrentFile,
+                    "Save As": () => {
+                        self.showSaveAs = true;
+                    }
+                },
+                Edit: {
+                    Undo: () => console.log("undo"),
+                    Redo: () => console.log("redo")
+                }
+            },
+            saveAsMessage: "",
+            showSaveAs: false,
+            initialSaveAsButtons: [
+                {
+                    text: "Cancel",
+                    onclick: function() {
+                        self.saveAsName = self.initialSaveAsName;
+                        self.showSaveAs = false;
+                    }
+                },
+                {
+                    text: "Save",
+                    onclick: function() {
+                        switch (self.saveAsName) {
+                            case "":
+                                self.saveAsMessage = "Path cannot be blank";
+                                break;
+                            case self.initialSaveAsName:
+                                self.saveCurrentFile();
+                                self.showSaveAs = false;
+                                break;
+                            default:
+                                // Check if the path is valid
+                                const pathDir = path.dirname(self.saveAsName);
+                                let dirExists = false;
+                                let fileExists = false;
+                                const projectPath =
+                                    self.$store.state.projectInfo.rootDirectory
+                                        .path;
+                                self.$store.state.projectInfo.mapProjectInfo(
+                                    fileObject => {
+                                        const fullDirPath = pathDir === "."
+                                            ? projectPath
+                                            : projectPath +
+                                                  self.delimiter +
+                                                  pathDir;
+                                        if (fileObject.path === fullDirPath) {
+                                            dirExists = true;
+                                        }
+                                        if (
+                                            fileObject.path ===
+                                            projectPath +
+                                                self.delimiter +
+                                                self.saveAsName
+                                        ) {
+                                            fileExists = true;
+                                        }
+                                    }
+                                );
+                                // If the directory does not exist, show an error
+                                if (!dirExists) {
+                                    self.saveAsMessage =
+                                        "Path does not point to a valid directory. You may need to create a new directory.";
+                                    break;
+                                }
+                                // If the file already exists, ask for confirmation
+                                self.confirmMessage = true;
+                                self.saveAsButtons = [
+                                    {
+                                        text: "Cancel",
+                                        onclick: function() {
+                                            self.saveAsName =
+                                                self.initialSaveAsName;
+                                            self.showSaveAs = false;
+                                            self.saveAsButtons =
+                                                self.initialSaveAsButtons;
+                                            self.confirmMessage = false;
+                                        }
+                                    },
+                                    {
+                                        text: "Confirm",
+                                        onclick: function() {
+                                            self.saveFileAs(self.saveAsName);
+                                            self.showSaveAs = false;
+                                            self.saveAsButtons =
+                                                self.initialSaveAsButtons;
+                                            self.confirmMessage = false;
+                                        }
+                                    }
+                                ];
+                                break;
+                        }
+                    }
+                }
+            ],
+            saveAsButtons: [],
+            saveAsName: self.initialSaveAsName,
+            delimiter: "/"
+        };
+    },
+    methods: {
+        saveCurrentFile() {
+            const self = this;
 
-             const projectPath = self.$store.state.projectInfo.rootDirectory.path;
-             const filepath = projectPath + this.delimiter + filename;
-             io.emit("write",
-                 {
-                     file: filepath,
-                     credentials: self.$store.state.credentials
-                 },
-                 (response) => {
-                     // TODO this won't work - I need to make a new file object
-                     self.$store.commit("setCurrentFile", filepath);
-                     if (response.status !== statusCodes.OKAY) {
-                         console.error(response);
-                     }
-                 });
-         }
-     },
-     mounted() {
-         this.saveAsButtons = this.initialSaveAsButtons;
-     },
-     watch: {
-         initialSaveAsName() {
-             this.saveAsName = this.initialSaveAsName;
-         }
-     }
- }
+            io.emit(
+                "write",
+                {
+                    file: self.currentFile.path,
+                    credentials: self.$store.state.credentials
+                },
+                response => {
+                    if (response.status !== statusCodes.OKAY) {
+                        console.error(response);
+                    }
+                }
+            );
+        },
+        saveFileAs(filename) {
+            const self = this;
+
+            const projectPath =
+                self.$store.state.projectInfo.rootDirectory.path;
+            const filepath = projectPath + this.delimiter + filename;
+            io.emit(
+                "write",
+                {
+                    file: filepath,
+                    credentials: self.$store.state.credentials
+                },
+                response => {
+                    // TODO this won't work - I need to make a new file object
+                    self.$store.commit("setCurrentFile", filepath);
+                    if (response.status !== statusCodes.OKAY) {
+                        console.error(response);
+                    }
+                }
+            );
+        }
+    },
+    mounted() {
+        this.saveAsButtons = this.initialSaveAsButtons;
+    },
+    watch: {
+        initialSaveAsName() {
+            this.saveAsName = this.initialSaveAsName;
+        }
+    }
+};
 </script>
 <style lang="scss">
  @import "../../scss/grid.scss";
